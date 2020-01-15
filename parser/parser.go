f8392e9ad84b80913d65c7daba472d09f2277585
DerEnderKeks <derenderkeks@gmail.com>
2020-01-15 09:34:20 +0100

    Fixed #13

--- parser/parser.go
+++ parser/parser.go
@@ -20,9 +20,9 @@
 package parser
 
 import (
+	"github.com/DerEnderKeks/LoginNotifier/log"
 	"regexp"
 	"time"
-	"github.com/DerEnderKeks/LoginNotifier/log"
 )
 
 type Session struct {
@@ -65,12 +65,12 @@ func (s Session) Time() time.Time {
 }
 
 func (s Session) String() string {
-	return s.User() + ", " + s.IP() + ", " + s.Host() + ", " + s.Time().Format(time.Stamp)
+	return s.User() + ", " + s.IP() + ", " + s.Host() + ", " + s.Time().Format(time.RFC3339)
 }
 
-var pubkeyRegex = regexp.MustCompile(`^(?P<time>([A-Z][a-z]{2})\s+[0-9]{1,2}\s+([0-9]{2}:){2}[0-9]{2})\s+(?P<host>\w+)\s+(\w+\[[0-9]+]:)\s+Accepted publickey for (?P<user>\w+) from (?P<ip>((:?[A-Fa-f0-9]{0,4})+|([0-9]{1,3}\.?)+))\s`)
+var pubkeyRegex = regexp.MustCompile(`^(?P<time>(([A-Z][a-z]{2})\s+[0-9]{1,2}\s+([0-9]{2}:?){3})|\d{4}-(\d{2}-?){2}T(\d{2}:?){3}.\d+[+-Z]((\d{2}:?){2})?)\s+(?P<host>\w+)\s+(\w+\[[0-9]+]:)\s+Accepted publickey for (?P<user>\w+) from (?P<ip>((:?[A-Fa-f0-9]{0,4})+|([0-9]{1,3}\.?)+))\s`)
 
-func ParseLine(line string) (*Session) {
+func ParseLine(line string) *Session {
 	var session Session
 	match := pubkeyRegex.FindStringSubmatch(line)
 	if match == nil {
@@ -80,8 +80,11 @@ func ParseLine(line string) (*Session) {
 		if i != 0 && name != "" {
 			switch name {
 			case "time":
-				sessiontime, _ := time.Parse(time.Stamp, match[i])
-				session.SetTime(sessiontime)
+				sessionTime, err := time.Parse(time.Stamp, match[i])
+				if err != nil {
+					sessionTime, _ = time.Parse(time.RFC3339, match[i])
+				}
+				session.SetTime(sessionTime)
 			case "host":
 				session.SetHost(match[i])
 			case "user":
